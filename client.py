1ce2365c610a423e30eb5fb70900ec3e0471ad9f
Muriel Smeets <mu.smeets@student.helmo.be>
2026-02-11 15:30:48 +0100

    Création du thread outbound message

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import logging
-from queue import Queue
+from queue import Queue, Empty
 from threading import Thread
 
 from secsychat_tui import SecsyChatTui, TuiMessage
@@ -12,6 +12,36 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+def handle_outbound_messages(q_outbound: Queue[TuiMessage]):
+    """
+    Fonction exécutée dans un thread pour traiter les messages envoyés depuis l'interface.
+    """
+
+    logger.info("Thread de traitement des messages sortants démarre")
+
+    while not q_outbound.is_shutdown:
+        try:
+            # Récupération d'un message avec timeout de 0.5 seconde
+            message = q_outbound.get(timeout=0.5)
+            # Traitement du message (affichage dans les logs pour l'instant)
+            logger.info(f"Message envoyé: {message}")
+
+            # Ici on ajoutera notre logique de traitement :
+            # - Envoi vers un serveur
+            # - Traitement cryptographique
+            # - Sauvegarde dans une base de données
+            # etc
+
+            # Marquage du message comme traité
+            q_outbound.task_done()
+        except Empty:
+            # Timeout atteint, on reboucle pour vérifier is_shutdown
+            continue
+        except Exception as e:
+            logger.error(f"Erreur lors du traitement d'un message sortant: {e}")
+
+    logger.info("Thread de traitement des messages sortants s'arrête")
+
 def main():
     logger.info("Demarrage de l'application")
 
@@ -19,29 +49,41 @@ def main():
     try:
         q_inbound = Queue[TuiMessage]()
         logger.debug("Queue inbound initialisee")
-    except:
-        logger.error("Erreur de queue inbound")
+    except Exception as e:
+        logger.error(f"Erreur de queue inbound: {e}")
 
     # Initialisation de la queue pour les messages envoyés depuis l'interface
     try:
         q_outbound = Queue[TuiMessage]()
         logger.debug("Queue outbound initialisee")
-    except:
-        logger.error("Erreur de queue outbound")
+    except Exception as e:
+        logger.error(f"Erreur de queue outbound: {e}")
 
     # Initialisation de l'interface (TUI)
     try:
         tui = SecsyChatTui(q_inbound, q_outbound, version="0.1.0")
         logger.info("Interface TUI initialisee")
-    except:
-        logger.error("Erreur de l'initialisation de la queue TUI")
+    except Exception as e:
+        logger.error(f"Erreur de l'initialisation de la TUI: {e}")
 
     # Création et lancement d'un thread permettant de gérer les messages envoyés
     try:
-        a = 1 #A effacer
-    except:
-        logger.error("Thread d'envoi non correctement implémente")
+        # Configuration du thread 
+        # - target = fonction à exécuter
+        # - args = arguments à passer à cette fonction sous forme de tuple
+        # - daemon = True -> thread s'arrêtera automatiquement quand le programme principal se termine
 
+        outbound_thread = Thread(
+            target=handle_outbound_messages,
+            args=(q_outbound,),
+            daemon=True,
+        )
+        outbound_thread.start()
+        
+        logger.info("Thread de traitement des messages sortants lancé")
+
+    except Exception as e:
+        logger.error(f"Thread d'envoi non correctement implémenté: {e}")
 
     # Exécution de l'interface de chat (TUI)
     tui.run()
@@ -55,6 +97,11 @@ def main():
     if not q_outbound.is_shutdown:
         q_outbound.shutdown(immediate=True)
         logger.debug("Queue outbound arretee")
+
+    # Attente de la fin du thread (join arrête le thread, timeout pour être sur qu'il n'attend pas indéfiniement)
+    if outbound_thread.is_alive():
+        outbound_thread.join(timeout=5.0)
+        logger.debug("Thread outbound termine")
 
 
 if __name__ == "__main__":
