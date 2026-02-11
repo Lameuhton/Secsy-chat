46c6dcd0497a42baa1e7d5b484f2fef0e8daf66d
Muriel Smeets <mu.smeets@student.helmo.be>
2026-02-11 14:23:31 +0100

    Création du log

--- client.py
+++ client.py
@@ -4,29 +4,57 @@ from threading import Thread
 
 from secsychat_tui import SecsyChatTui, TuiMessage
 
+# CONFIGURATION DU LOGGER
+logging.basicConfig(
+    filename="app.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 def main():
+    logger.info("Demarrage de l'application")
+
     # Initialisation de la queue pour les messages reçus à afficher dans l'interface
-    q_inbound = Queue[TuiMessage]()
+    try:
+        q_inbound = Queue[TuiMessage]()
+        logger.debug("Queue inbound initialisee")
+    except:
+        logger.error("Erreur de queue inbound")
 
     # Initialisation de la queue pour les messages envoyés depuis l'interface
-    q_outbound = Queue[TuiMessage]()
+    try:
+        q_outbound = Queue[TuiMessage]()
+        logger.debug("Queue outbound initialisee")
+    except:
+        logger.error("Erreur de queue outbound")
 
     # Initialisation de l'interface (TUI)
-    tui = SecsyChatTui(q_inbound, q_outbound, version="0.1.0")
+    try:
+        tui = SecsyChatTui(q_inbound, q_outbound, version="0.1.0")
+        logger.info("Interface TUI initialisee")
+    except:
+        logger.error("Erreur de l'initialisation de la queue TUI")
 
     # Création et lancement d'un thread permettant de gérer les messages envoyés
-    # TODO
+    try:
+        a = 1 #A effacer
+    except:
+        logger.error("Thread d'envoi non correctement implémente")
+
 
     # Exécution de l'interface de chat (TUI)
     tui.run()
+    logger.info("Lancement de l'interface")
 
     # Mise en arrêt des queues pour les messages reçus à afficher et les messages envoyés depuis l'interface
     if not q_inbound.is_shutdown:
         q_inbound.shutdown(immediate=True)
+        logger.debug("Queue inbound arretee")
 
     if not q_outbound.is_shutdown:
         q_outbound.shutdown(immediate=True)
+        logger.debug("Queue outbound arretee")
 
 
 if __name__ == "__main__":
