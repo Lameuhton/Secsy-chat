e3fa7aee626edddc7db6ba35b0eccbe6267e2593
Auréliane Gagliardi <a.gagliardi@student.helmo.be>
2026-02-23 12:51:45 +0100

    Config logger dans server.py

--- server.py
+++ server.py
@@ -1,5 +1,20 @@
+from common import network
+import logging
+
+# CONFIGURATION DU LOGGER
+logging.basicConfig(
+    filename="app_server.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger_client = logging.getLogger(__name__)
+
 def main():
-    # TODO
+
+    sock = network.start_tcp_server("127.0.0.1", 4000)
+
+    while True:
+
 
 if __name__ == "__main__":
     main()
