864299eb7c0b1647dfc92aff3ec3dbf4593fec91
Auréliane Gagliardi <a.gagliardi@student.helmo.be>
2026-02-23 13:55:23 +0100

    Gestion connexion et messages server.py

--- server.py
+++ server.py
@@ -11,11 +11,24 @@ logger_server = logging.getLogger(__name__)
 
 def main():
 
-    sock = network.start_tcp_server("127.0.0.1", 4000)
+    sock_server = network.start_tcp_server("127.0.0.1", 4000)
 
     while True:
-        sock, addr = sock.accept()
+        # Attente connexion client
+        sock_client, addr = sock_server.accept()
+        logger_server.info(f"Connexion acceptee de {addr}")
 
+        # Connexion avec client
+        while True:
+            message = network.receive_message_as_str(sock_client)
+            if not message: #Client déconnecté
+                break
+            # Renvoie le message au client pour affichage
+            network.send_message_as_str(sock_client, message)
+
+        # Client déconnecté, on ferme le socket client
+        sock_client.close()
+        logger_server.info(f"Client {addr} deconnecte, attente d'une nouvelle connexion...")
 
 if __name__ == "__main__":
     main()
