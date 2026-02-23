f4b95af8c7e73b861017938c2d61c2ff03497afc
Auréliane Gagliardi <a.gagliardi@student.helmo.be>
2026-02-23 13:10:52 +0100

    modif server 1

--- server.py
+++ server.py
@@ -7,12 +7,14 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-logger_client = logging.getLogger(__name__)
+logger_server = logging.getLogger(__name__)
 
 def main():
 
     sock = network.start_tcp_server("127.0.0.1", 4000)
 
+    while True:
+        sock, addr = sock.accept()
 
 
 if __name__ == "__main__":
