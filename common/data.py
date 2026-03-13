a1e4ae5f952ce4feab3382e09b486027115ff6fa
Muriel Smeets <mu.smeets@student.helmo.be>
2026-03-13 11:05:26 +0100

    Première fonction data.py

--- common/data.py
+++ common/data.py
@@ -1,11 +1,24 @@
+import database
+
+#!!!!!!!!!!!!!!!!!!!!
+DB_PATH = "database.db"
+#!!!!!!!!!!!!!!!!!!!!
+
 def user_exists(name: str) -> bool:
     """
     Détermine l'existence d'un utilisateur sur base de son nom (pseudonyme)
-    :param name le nom de l'utilisateur (pseudonyme)
-    :return `True` si un utilisateur existe sur base du nom fourni, sinon `False`
+    :param name: le nom de l'utilisateur (pseudonyme)
+    :return: `True` si un utilisateur existe sur base du nom fourni, sinon `False`
     """
-
-
+    connexion = database.connect_to_db(DB_PATH)
+    cursor = connexion.cursor()
+    cursor.execute("SELECT * FROM users WHERE name = ?", (name,))
+    resultat = cursor.fetchall() # Prend les derniers résultats de la dernière requête exécutée
+    database.close_connection(connexion)
+    if not resultat:
+        return False
+    else:
+        return True
 #
 # Remplacez ce commentaire par vos propres fonctions d'accès et de modification de données
 #
