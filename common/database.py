e1df2606bf52af04f36486ac226a2f100f8b5118
Muriel Smeets <mu.smeets@student.helmo.be>
2026-03-13 08:36:21 +0100

    correction des docstrings et modif de diffie hellman compute

--- common/database.py
+++ common/database.py
@@ -5,8 +5,8 @@ from typing import List, Tuple
 def connect_to_db(path: str) -> Connection:
     """
     Se connecte à une base de données SQLite et retourne la connection créée
-    :param path le chemin vers la base de données SQLite
-    :return la connection à la base de données
+    :param path: le chemin vers la base de données SQLite
+    :return: la connection à la base de données
     """
     #NB: SQLite est une base de donnée stockée dans un simple fichier sur le disque
     return connect(path)
@@ -14,7 +14,7 @@ def connect_to_db(path: str) -> Connection:
 def close_connection(connection: Connection):
     """
     Ferme la connexion à la base de données
-    :param connection la connexion à fermer
+    :param connection: la connexion à fermer
     """
     connection.close()
     #La fonction ne demande aucun return du coup pas de return
@@ -22,10 +22,10 @@ def close_connection(connection: Connection):
 def insert_data(connection: Connection, table: str, columns: Tuple, data: Tuple):
     """
     Insère un tuple au sein d'une table en renseignant les valeurs (data) des colonnes et en utilisant une connexion existante
-    :param connection la connexion existante à la base de données
-    :param table la table dans laquelle insérer les données
-    :param columns le nom des colonnes pour lesquelles des données sont insérées
-    :param data la valeur des colonnes à insérer
+    :param connection: la connexion existante à la base de données
+    :param table: la table dans laquelle insérer les données
+    :param columns: le nom des colonnes pour lesquelles des données sont insérées
+    :param data: la valeur des colonnes à insérer
     """
     #transformer le tuple columns en tuple str
     colonnes = ", ".join(columns)
@@ -36,25 +36,26 @@ def insert_data(connection: Connection, table: str, columns: Tuple, data: Tuple)
     # du coup on mes data en valeurs à substituer
     # (f" car il y a des variables dedans
     cursor.execute(f"INSERT INTO {table} ({colonnes}) VALUES ({secu_injection})", data) # Entre accolades pour dire que ce sont des arguments importés de python
+    #commit = ctrl+S
     connection.commit()
 
 def select_data(connection: Connection, query: str) -> List[Tuple]:
     """
     Sélectionne des tuples depuis la base de donnée (connection) et retourne les enregistrements correspondants
-    :param connection la connexion déjà établie à la base de données
-    :param query la requête SQL à exécuter sur la base de données (connection)
-    :return les enregistrements correspondants au résultat de la reqête
+    :param connection: la connexion déjà établie à la base de données
+    :param query: la requête SQL à exécuter sur la base de données (connection)
+    :return: les enregistrements correspondants au résultat de la reqête
     """
 
 def execute_script(connection, path: str):
     """
     Exécute un script directement en base de données en utilisant une connexion existante
-    :param connection la connexion existante à la base de données
-    :param path le chemin vers le fichier contenant le script SQL
+    :param connection: la connexion existante à la base de données
+    :param path: le chemin vers le fichier contenant le script SQL
     """
 
 def execute_seed(connection):
     """
     Si elles n'existent pas, crée les tables, les colonnes et contraintes associées et insère les données nécessaires
-    :param connection la connexion existante à la base de données
+    :param connection: la connexion existante à la base de données
     """
