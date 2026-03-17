79443e38d5614e3eb565df6fa8ab87851511aed8
Muriel Smeets <mu.smeets@student.helmo.be>
2026-03-17 14:23:17 +0100

    Commentaires data

--- common/data.py
+++ common/data.py
@@ -10,15 +10,16 @@ def user_exists(name: str) -> bool:
     :param name: le nom de l'utilisateur (pseudonyme)
     :return: `True` si un utilisateur existe sur base du nom fourni, sinon `False`
     """
-    connexion = database.connect_to_db(DB_PATH)
+    connexion = database.connect_to_db(DB_PATH) # Indique au passage à la fonction qu'on utilise sqlite3 grâce à database
     cursor = connexion.cursor()
-    cursor.execute("SELECT * FROM users WHERE name = ?", (name,))
+    cursor.execute("SELECT * FROM users WHERE name = ?", (name,)) # Il y a une virgule après name pourqu'il soit considéré comme un tuple (exécute utilise un tuple)
     resultat = cursor.fetchall() # Prend les derniers résultats de la dernière requête exécutée
     database.close_connection(connexion)
     if not resultat:
         return False
     else:
         return True
+
 #
 # Remplacez ce commentaire par vos propres fonctions d'accès et de modification de données
 #
